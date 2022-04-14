52ac571598e8fb42218f9b0f73010db434744c5b
Thrishul <thrishulishere@gmail.com>
2022-04-14 22:46:54 +0530

    player movement changes

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,18 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
+const MaxSpeed = 100
+const Acceleration = 25
+const friction = 50
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
-	move_and_collide(input_vector)
+	input_vector = input_vector.normalized();
+	if input_vector == Vector2.ZERO:
+		velocity = velocity.move_toward(Vector2.ZERO, friction)
+	else:
+		velocity += input_vector * Acceleration * delta
+		velocity = velocity.clamped(MaxSpeed * delta)
+	move_and_collide(velocity);
